3c3a1cd921f9a4c99f64b5dc2ad5f577c0b9750d
ExellentCoin2044 <caluwejonas@hotmail.com>
2019-06-21 21:28:29 +0200

    initial commit

--- JavaScript/ProjectCreator/app.js
+++ JavaScript/ProjectCreator/app.js
@@ -4,6 +4,11 @@ const parameters = process.argv;
 
 function createProject() {
     mkdirp(`D:/Jos/Bureaublad/Jonas/MyProjects/GitProjects/${parameters[2]}/${parameters[3]}`);
+    //exec(`cd /d D:/Jos/Bureaublad/Jonas/MyProjects/GitProjects/${parameters[2]}/${parameters[3]}`, (err, stdout, stderr) => {process.stdout.write(stdout);});
+    //exec('touch app.js', (err, stdout, stderr) => {process.stdout.write(stdout);});
+    exec(`git add -A`, (err, stdout, stderr) => {process.stdout.write(stdout);});
+    exec(`git commit -m "initial commit"`, (err, stdout, stderr) => {process.stdout.write(stdout);});
+    exec('git push', (err, stdout, stderr) => {process.stdout.write(stdout);});
 }
 
 createProject();
