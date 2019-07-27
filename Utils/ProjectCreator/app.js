781991506825ec9943fd4c441ac4f7120a2f3a3d
ExellentCoin2044 <caluwejonas@hotmail.com>
2019-07-27 17:14:20 +0200

    auto-commit

--- Utils/ProjectCreator/app.js
+++ Utils/ProjectCreator/app.js
@@ -2,31 +2,48 @@ const exec = require('child_process').exec;
 const mkdirp = require('mkdirp');
 const push = require('git-push');
 const fs = require('fs');
+
+
 const parameters = process.argv;
 const path = `D:/Jos/Bureaublad/Jonas/MyProjects/GitProjects/${parameters[2]}/${parameters[3]}`
+let isNode;
+
 
 function createProject() {
 
+    //make a dir with the specified path
     mkdirp(path);
 
-    makeFile();
+    initProject();
 
-    //checks what the first parameter is, if it is react then init the react project if it is node do an npm init
-    check();
-
-    gitInit()
+    // gitInit()
 }
 
-function makeFile() {
+// function check() {
+//     (parameters[2] === 'Nodejs') ? isNode = true: null;
+// }
+
+function initProject() {
+    //checking if the first parameter is Node. If it is, it will init
+    if (parameters[2] === 'Nodejs') {
+        exec(`npm init -y`, (err, stdout, stderr) => {
+            process.stdout.write(stdout);
+        });
+    }
+
+    if (parameters[2] === 'React') {
+        exec(`cd d:/Jos/Bureaublad/Jonas/MyProjects/gitProjects/React/${parameters[3]} && npx create-react-app ${parameters[4]} -y`, (err, stdout, stderr) => {
+            process.stdout.write(stdout);
+        });
+
+        return;
+    }c
+
     fs.writeFile(`${path}/app.js`, "init", function (err) {
         if (err) {
             return console.log(err);
         }
     });
-}
-
-function check() {
-
 }
 
 function gitInit() {
